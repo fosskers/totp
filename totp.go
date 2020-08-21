f2f84fc57691c61cf52b6946f544b3b3153deb2e
Colin Woodbury <colin@fosskers.ca>
2020-08-21 13:48:42 -0700

    `toBytes` and tests

--- totp.go
+++ totp.go
@@ -1,5 +1,23 @@
 package totp
 
+// 30 seconds.
+const DEFAULT_STEP uint64 = 30
+
+// 8 digits of output.
+const DEFAULT_DIGITS uint32 = 8
+
 func Totp(password []byte, time uint64) string {
 	return ""
+}
+
+// Convert an unsigned 64-bit int into its 8 individual bytes.
+func toBytes(n uint64) [8]byte {
+	const mask uint64 = 0x00000000000000ff
+	bytes := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
+
+	for i := range bytes {
+		bytes[7-i] = byte(mask & (n >> (i * 8)))
+	}
+
+	return bytes
 }
---
+++ totp_test.go
@@ -0,0 +1,23 @@
+package totp
+
+import "testing"
+
+func TestToBytes(t *testing.T) {
+	expected := [8]byte{0, 0, 0, 0, 0, 0, 0, 1}
+	result := toBytes(59 / DEFAULT_STEP)
+	if result != expected {
+		t.Errorf("toBytes: expected ?, got ?")
+	}
+
+	expected = [8]byte{0, 0, 0, 0, 0x02, 0x35, 0x23, 0xec}
+	result = toBytes(1111111109 / DEFAULT_STEP)
+	if result != expected {
+		t.Errorf("toBytes: expected ?, got ?")
+	}
+
+	expected = [8]byte{0, 0, 0, 0, 0x27, 0xbc, 0x86, 0xaa}
+	result = toBytes(20000000000 / DEFAULT_STEP)
+	if result != expected {
+		t.Errorf("toBytes: expected ?, got ?")
+	}
+}
