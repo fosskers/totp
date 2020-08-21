2f78df4b18f57d6809be6735409647ef0f017212
Colin Woodbury <colin@fosskers.ca>
2020-08-21 15:31:23 -0700

    All Sha512 tests pass

--- totp_test.go
+++ totp_test.go
@@ -19,17 +19,25 @@ func TestToBytes(t *testing.T) {
 	testBytes(t, [8]byte{0, 0, 0, 0, 0x27, 0xbc, 0x86, 0xaa}, toBytes(20000000000/DEFAULT_STEP))
 }
 
+func testTotp(t *testing.T, secret []byte, time uint64, expected string) {
+	hash := Totp(secret, time)
+	if hash != expected {
+		t.Errorf("Totp: expected %s, got %s", expected, hash)
+	}
+}
+
 func TestTotp(t *testing.T) {
 	secret := []byte("1234567890123456789012345678901234567890123456789012345678901234")
 	if len(secret) != 64 {
 		t.Errorf("Byte Conversion: expected 64, got %d", len(secret))
 	}
 
-	hash := Totp(secret, 59)
-	expected := "90693936"
-	if hash != "90693936" {
-		t.Errorf("Totp: expected %s, got %s", expected, hash)
-	}
+	testTotp(t, secret, 59, "90693936")
+	testTotp(t, secret, 1111111109, "25091201")
+	testTotp(t, secret, 1111111111, "99943326")
+	testTotp(t, secret, 1234567890, "93441116")
+	testTotp(t, secret, 2000000000, "38618901")
+	testTotp(t, secret, 20000000000, "47863826")
 }
 
 // --- Documentation Examples --- //
