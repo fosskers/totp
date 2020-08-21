31a6650d62a21304270514e3f965fdc4448e6ed0
Colin Woodbury <colin@fosskers.ca>
2020-08-21 15:52:40 -0700

    Algorithm configuration and Sha256 tests

--- totp.go
+++ totp.go
@@ -19,8 +19,8 @@ package totp
 
 import (
 	"crypto/hmac"
-	"crypto/sha512"
 	"fmt"
+	"hash"
 )
 
 // DEFAULT_STEP represents a 30 second time step.
@@ -29,11 +29,19 @@ const DEFAULT_STEP uint64 = 30
 // DEFAULT_DIGITS represents 8 digits of final output.
 const DEFAULT_DIGITS uint32 = 8
 
-// Totp is a cool function that orders you lunch. Yes
-func Totp(password []byte, time uint64) string {
+// Totp produces a Time-based One-time Password with default settings.
+//
+// mac := hmac.New(sha512.New, password)
+func Totp(h func() hash.Hash, password []byte, time uint64) string {
+	return TotpCustom(h, DEFAULT_STEP, DEFAULT_DIGITS, password, time)
+}
+
+// TotpCustom produces a Time-based One-time Password with full control over
+// algorithm parameters.
+func TotpCustom(h func() hash.Hash, step uint64, digits uint32, password []byte, time uint64) string {
 	// Hash the secret and the time together.
-	mac := hmac.New(sha512.New, password)
-	bytes := toBytes(time / DEFAULT_STEP)
+	mac := hmac.New(h, password)
+	bytes := toBytes(time / step)
 	mac.Write(bytes[:])
 	hash := mac.Sum(nil)
 
@@ -41,7 +49,7 @@ func Totp(password []byte, time uint64) string {
 	offset := hash[len(hash)-1] & 0xf
 	binary := (uint64(hash[offset]&0x7f) << 24) | (uint64(hash[offset+1]) << 16) | (uint64(hash[offset+2]) << 8) | uint64(hash[offset+3])
 
-	return fmt.Sprintf("%08d", binary%100000000)
+	return fmt.Sprintf("%0[2]*[1]d", binary%100000000, digits)
 }
 
 // Convert an unsigned 64-bit int into its 8 individual bytes.
--- totp_test.go
+++ totp_test.go
@@ -1,7 +1,10 @@
 package totp
 
 import (
+	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
+	"hash"
 	"testing"
 )
 
@@ -19,25 +22,39 @@ func TestToBytes(t *testing.T) {
 	testBytes(t, [8]byte{0, 0, 0, 0, 0x27, 0xbc, 0x86, 0xaa}, toBytes(20000000000/DEFAULT_STEP))
 }
 
-func testTotp(t *testing.T, secret []byte, time uint64, expected string) {
-	hash := Totp(secret, time)
+func testTotp(t *testing.T, h func() hash.Hash, secret []byte, time uint64, expected string) {
+	hash := Totp(h, secret, time)
 	if hash != expected {
 		t.Errorf("Totp: expected %s, got %s", expected, hash)
 	}
 }
 
-func TestTotp(t *testing.T) {
+func TestTotp256(t *testing.T) {
+	secret := []byte("12345678901234567890123456789012")
+	if len(secret) != 32 {
+		t.Errorf("Byte Conversion: expected 32, got %d", len(secret))
+	}
+
+	testTotp(t, sha256.New, secret, 59, "46119246")
+	testTotp(t, sha256.New, secret, 1111111109, "68084774")
+	testTotp(t, sha256.New, secret, 1111111111, "67062674")
+	testTotp(t, sha256.New, secret, 1234567890, "91819424")
+	testTotp(t, sha256.New, secret, 2000000000, "90698825")
+	testTotp(t, sha256.New, secret, 20000000000, "77737706")
+}
+
+func TestTotp512(t *testing.T) {
 	secret := []byte("1234567890123456789012345678901234567890123456789012345678901234")
 	if len(secret) != 64 {
 		t.Errorf("Byte Conversion: expected 64, got %d", len(secret))
 	}
 
-	testTotp(t, secret, 59, "90693936")
-	testTotp(t, secret, 1111111109, "25091201")
-	testTotp(t, secret, 1111111111, "99943326")
-	testTotp(t, secret, 1234567890, "93441116")
-	testTotp(t, secret, 2000000000, "38618901")
-	testTotp(t, secret, 20000000000, "47863826")
+	testTotp(t, sha512.New, secret, 59, "90693936")
+	testTotp(t, sha512.New, secret, 1111111109, "25091201")
+	testTotp(t, sha512.New, secret, 1111111111, "99943326")
+	testTotp(t, sha512.New, secret, 1234567890, "93441116")
+	testTotp(t, sha512.New, secret, 2000000000, "38618901")
+	testTotp(t, sha512.New, secret, 20000000000, "47863826")
 }
 
 // --- Documentation Examples --- //
